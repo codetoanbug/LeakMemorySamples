e4bf36958dca78a26e4914f8775cdb8293b64847
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-04-14 16:14:47 +0900

    fix

--- LeakMemorySample/Singleton Leak View/SingletonClass/AlertViewHandleLogic.swift
+++ LeakMemorySample/Singleton Leak View/SingletonClass/AlertViewHandleLogic.swift
@@ -7,24 +7,55 @@
 
 import Foundation
 
+
+protocol AlertViewHandleLogicDelegate: AnyObject {
+    // Show alert view
+    func needShowAlertView(sender: AlertViewHandleLogic)
+}
+
 class AlertViewHandleLogic {
+    // Stores the list of observers to notify about server changes.
+    private var observers: [AlertViewHandleLogicDelegate] = []
+
     // For lock screen
     private var showLockPopupTimer: Timer?
     private var timeLockCountDown: TimeInterval
-    var needShowAlertView: (() -> Void)?
+//    var needShowAlertView: (() -> Void)?
 
     private init() {
         self.timeLockCountDown = 5
+        self.resetShowAlertTimer()
     }
 
     static let shared = AlertViewHandleLogic()
 
     /// Reset show alert timer
-    func resetShowAlertTimer() {
+    private func resetShowAlertTimer() {
         showLockPopupTimer?.invalidate()
         showLockPopupTimer = Timer.scheduledTimer(withTimeInterval: timeLockCountDown, repeats: true, block: { [weak self] _ in
             guard let strongSelf = self else { return }
-            strongSelf.needShowAlertView?()
+            // Step call back
+//            strongSelf.needShowAlertView?()
+
+            // Step observer
+            strongSelf.observers.forEach({ observer in
+                observer.needShowAlertView(sender: strongSelf)
+            })
         })
+    }
+
+    /// Add observer to listen action
+    /// - Parameter observer: action delegate
+    func addObserver(_ observer: AlertViewHandleLogicDelegate) {
+        if !self.observers.contains(where: { $0 === observer }) {
+            self.observers.append(observer)
+        }
+    }
+
+    /// Remove observer in a view  or view controller
+    /// - Parameter observer: observer from view need remove
+    func removeObserver(_ observer: AlertViewHandleLogicDelegate) {
+        guard let index = self.observers.firstIndex(where: { return $0 === observer }) else { return }
+        self.observers.remove(at: index)
     }
 }
--- LeakMemorySample/Singleton Leak View/SingletonLeakViewController.swift
+++ LeakMemorySample/Singleton Leak View/SingletonLeakViewController.swift
@@ -8,16 +8,36 @@
 import UIKit
 
 class SingletonLeakViewController: BaseViewController {
-    var alertViewHandleLogic: AlertViewHandleLogic!
+//    var alertViewHandleLogic: AlertViewHandleLogic!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        alertViewHandleLogic = AlertViewHandleLogic.shared
-        alertViewHandleLogic.needShowAlertView = { [weak self] in
-            print("need show here")
-        }
+//        alertViewHandleLogic = AlertViewHandleLogic.shared
+        // Step cakkback by closure
+//        alertViewHandleLogic.needShowAlertView = { [weak self] in
+//            print("need show here")
+//        }
 
-        alertViewHandleLogic.resetShowAlertTimer()
+        AlertViewHandleLogic.shared.addObserver(self)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        AlertViewHandleLogic.shared.removeObserver(self)
+    }
+
+    deinit {
+        NSLog("free memory SingletonLeakViewController")
+    }
+
+    /// Remove observers
+    func removeObservers() {
+        AlertViewHandleLogic.shared.removeObserver(self)
+    }
+}
+
+extension SingletonLeakViewController: AlertViewHandleLogicDelegate {
+    func needShowAlertView(sender: AlertViewHandleLogic) {
+        print("need show here")
+    }
 }
